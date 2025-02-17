7132911ff2aaca4f5f4107244d6e6cd2e439c22e
Samanthinha <samanthapcaetano809@gmail.com>
2025-02-17 20:16:54 -0300

    jogo da velha muito top

--- jogodavelha.py
+++ jogodavelha.py
@@ -1,61 +1,42 @@
-def criar_tabuleiro():
-    return [[' ' for _ in range(3)] for _ in range(3)]
-
-def exibir_tabuleiro(tabuleiro):
-    for linha in tabuleiro:
-        print('|'.join(linha))
-        print('-' * 5)
-
-def verificar_vitoria(tabuleiro, jogador):
-    for linha in tabuleiro:
-        if all(celula == jogador for celula in linha):
-            return True
-    
-    for coluna in range(3):
-        if all(tabuleiro[linha][coluna] == jogador for linha in range(3)):
-            return True
-    
-    if all(tabuleiro[i][i] == jogador for i in range(3)) or all(tabuleiro[i][2 - i] == jogador for i in range(3)):
-        return True
-    
-    return False
-
-def verificar_empate(tabuleiro):
-    return all(celula != ' ' for linha in tabuleiro for celula in linha)
-
-def jogada_valida(tabuleiro, linha, coluna):
-    return 0 <= linha < 3 and 0 <= coluna < 3 and tabuleiro[linha][coluna] == ' '
+def exibir_tabuleiro():
+    print(tabuleiro1)
+    print(tabuleiro2)
+    print(tabuleiro3)
 
 def jogo_da_velha():
-    tabuleiro = criar_tabuleiro()
-    jogador_atual = 'X'
+    global tabuleiro1, tabuleiro2, tabuleiro3
+    tabuleiro1 = [" ", " ", " "]
+    tabuleiro2 = [" ", " ", " "]
+    tabuleiro3 = [" ", " ", " "]
+    jogador_atual = "X"
     
-    while True:
-        exibir_tabuleiro(tabuleiro)
-        try:
-            linha = int(input(f'Jogador {jogador_atual}, escolha a linha (0-2): '))
-            coluna = int(input(f'Jogador {jogador_atual}, escolha a coluna (0-2): '))
-        except ValueError:
-            print("Entrada inválida! Insira números entre 0 e 2.")
+    for _ in range(9):
+        exibir_tabuleiro()
+        linha = int(input("Linha (0-2): "))
+        coluna = int(input("Coluna (0-2): "))
+        
+        if linha == 0:
+            if tabuleiro1[coluna] != " ":
+                print("Posição ocupada!")
+                continue
+            tabuleiro1[coluna] = jogador_atual
+        elif linha == 1:
+            if tabuleiro2[coluna] != " ":
+                print("Posição ocupada!")
+                continue
+            tabuleiro2[coluna] = jogador_atual
+        elif linha == 2:
+            if tabuleiro3[coluna] != " ":
+                print("Posição ocupada!")
+                continue
+            tabuleiro3[coluna] = jogador_atual
+        else:
+            print("Linha inválida!")
             continue
+        
+        jogador_atual = "O" if jogador_atual == "X" else "X"
+    
+    exibir_tabuleiro()
+    print("Fim do jogo!")
 
-        if not jogada_valida(tabuleiro, linha, coluna):
-            print("Posição inválida ou já ocupada! Escolha outra.")
-            continue
-        
-        tabuleiro[linha][coluna] = jogador_atual
-        
-        if verificar_vitoria(tabuleiro, jogador_atual):
-            exibir_tabuleiro(tabuleiro)
-            print(f'Jogador {jogador_atual} venceu!')
-            break
-        
-        if verificar_empate(tabuleiro):
-            exibir_tabuleiro(tabuleiro)
-            print('O jogo empatou!')
-            break
-        
-        jogador_atual = 'O' if jogador_atual == 'X' else 'X'
-
-if __name__ == "__main__":
-    jogo_da_velha()
+jogo_da_velha()
